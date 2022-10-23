cbfd662e010927ec3c5628a178cc6f2bed7f452f
MDSABIRKHAN <113968742+MDSABIRKHAN@users.noreply.github.com>
2022-10-23 19:16:57 +0530

    Add files via upload

---
+++ src/com/project/Main.java
@@ -0,0 +1,11 @@
+package com.project;
+
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\khans\\OneDrive\\Desktop\\Simplilearn"; 
+	public static void main(String[] args) {
+	Menus menu = new Menus();
+	menu.introScreen();
+	menu.mainMenu();
+	}
+}
